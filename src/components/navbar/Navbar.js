c68490bf2f0cd3e7a8811c34d6e7386de9e1c1f2
MaximCheb <chebola.maxim@gmail.com>
2024-04-21 20:50:17 +0300

    change navbar to icons

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,15 +5,16 @@ import { useTranslation } from "react-i18next";
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import sun from "./../../images/icons/sun.svg";
-import moon from "./../../images/icons/sun.svg";
+import homeIcon from "./../../images/icons/home.svg";
+import cardIcon from "./../../images/icons/cards.svg";
+import contactsIcon from "./../../images/icons/chat.svg";
 
 import rus_lang from "./../../images/icons/rus-flag.svg";
 import en_lang from "./../../images/icons/us-flag.svg";
 
 function Navbar() {
     const [state, setState] = useState({ lang: "en" });
-    const { i18n } = useTranslation();
+    const { i18n, t } = useTranslation();
 
     const activeLink = 'nav-list__link nav-list__link--active';
     const normalLink = 'nav-list__link';
@@ -22,21 +23,15 @@ function Navbar() {
         setState((prevState) => {
             return { lang: prevState.lang === "en" ? "ru" : "en" };
         });
-
         i18n.changeLanguage(state.lang);
     };
-
     return (
         <nav className="nav">
             <div className="container">
                 <div className="nav-row">
                     <NavLink to="/" className="logo">
-						<strong>Freelancer</strong> portfolio
+						<strong>{t('title')}</strong>
 					</NavLink>
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon"></img>
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"></img>
-                    </button>
                     <button onClick={() => changeLanguage("en")} className="dark-mode-btn">
                         <img src={state.lang === "en" ? rus_lang : en_lang} alt="Light mode" className="dark-mode-btn__icon"></img>
                     </button>
@@ -50,18 +45,9 @@ function Navbar() {
                                     isActive ? activeLink : normalLink
                                 }
                             >
-                                Home
+                                <img src={homeIcon} className='nav-list__icon' alt='#'/>
+                                <div className='nav-list__text'>{t('NavbarHome')}</div>
                             </NavLink></li>
-                            {/* TODO: create page */}
-                        {/* <li className="nav-list__item">
-                            <NavLink
-                                to="/about"
-                                className={({ isActive }) =>
-                                    isActive ? activeLink : normalLink
-                                }
-                            >
-                                About
-                            </NavLink></li>     */}
                         <li className="nav-list__item">
                             <NavLink
                                 to="/projects"
@@ -69,7 +55,8 @@ function Navbar() {
                                     isActive ? activeLink : normalLink
                                 }
                             >
-                                Projects
+                                <img src={cardIcon} className='nav-list__icon' alt='#'/>
+                                <div className='nav-list__text'>{t('NavbarProjects')}</div>
                             </NavLink></li>
                         <li className="nav-list__item">
                             <NavLink
@@ -78,7 +65,8 @@ function Navbar() {
                                     isActive ? activeLink : normalLink
                                 }
                             >
-                                Contacts
+                                <img src={contactsIcon} className='nav-list__icon' alt='#'/>
+                                <div className='nav-list__text'>{t('NavbarContacts')}</div>
                             </NavLink>
                         </li>
                     </ul>
